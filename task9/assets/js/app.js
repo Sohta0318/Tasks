693e5d041412e6803d535d6f327abcef22b582bc
SOHTA0318 <nicosohta0318@gmail.com>
2022-01-26 12:56:14 -0800

    task 9

--- task9/assets/js/app.js
+++ task9/assets/js/app.js
@@ -1,77 +1,72 @@
-const avatar = document.querySelector("#avatar");
+const mario = document.querySelector("#avatar");
 const coin = document.querySelector("#coin");
 const sound = new Audio("assets/audio/smw_coin.wav");
-const p = document.createElement("p");
+const score = document.createElement("p");
 
-let hMove = 0,
-  vMove = 0;
-let score = 0;
-const offsetH = parseInt(window.getComputedStyle(coin, null).height);
-const offsetW = parseInt(window.getComputedStyle(coin, null).width);
+let move = 25;
+let scoreCount = 0;
+let marioY;
+let marioX;
+const randomScreenHeight = Math.floor(Math.random() * 900);
+const randomScreenWidth = Math.floor(Math.random() * 900);
+let coinY = 0;
+let coinX = 0;
 
-window.onload = function () {
-  init();
+const coinMove = () => {
+  coin.style.left = `${randomScreenHeight}px`;
+  coin.style.top = `${randomScreenWidth}px`;
+  coinY = Math.floor(randomScreenHeight / 10) * 25;
+  coinX = Math.floor(randomScreenWidth / 10) * 25;
 };
+window.addEventListener("load", () => {
+  mario.style.left = "100px";
+  mario.style.top = "100px";
+  coinMove();
+  console.log(coinX);
+});
 
-function isTouching(a, b) {
-  const aRect = a.getBoundingClientRect();
-  const bRect = b.getBoundingClientRect();
-  return !(
-    aRect.top + aRect.height < bRect.top ||
-    aRect.top > bRect.top + bRect.height ||
-    aRect.left + aRect.width < bRect.left ||
-    aRect.left > bRect.left + bRect.width
-  );
-}
+window.addEventListener("keydown", (e) => {
+  const keyDown = e.key;
+  const marioLeft = parseInt(mario.style.left) - move;
+  const marioRight = parseInt(mario.style.left) + move;
+  const marioUp = parseInt(mario.style.top) - move;
+  const marioDown = parseInt(mario.style.top) + move;
+  switch (keyDown) {
+    case "ArrowLeft":
+      mario.style.left = marioLeft + "px";
+      marioX = marioLeft - 100;
+      console.log(marioX);
+      check();
+      break;
+    case "ArrowRight":
+      mario.style.left = marioRight + "px";
+      marioX = marioRight - 100;
+      console.log(marioX);
+      check();
 
-const init = () => {
-  const aRect = avatar.getBoundingClientRect();
-  const height = Math.floor(Math.random() * (window.innerHeight - offsetH));
-  const width = Math.floor(Math.random() * (window.innerWidth - offsetW));
-
-  coin.style.left = `${width}px`;
-  coin.style.top = `${height}px`;
-
-  (hMove = aRect.left), (vMove = aRect.top);
-  p.innerHTML = `Score: ${score}`;
-  document.body.appendChild(p);
-};
-
-document.addEventListener("keydown", (event) => {
-  const aRect = avatar.getBoundingClientRect();
-
-  // left
-  if (event.keyCode === 37) {
-    if (aRect.left > 0) {
-      hMove -= 50;
-      avatar.style.left = `${hMove}px`;
-    }
-  }
-  // up
-  if (event.keyCode === 38) {
-    if (aRect.top > 0) {
-      vMove -= 50;
-      avatar.style.top = `${vMove}px`;
-    }
-  }
-  // right
-  if (event.keyCode === 39) {
-    if (aRect.left < window.innerWidth - offsetW) {
-      hMove += 50;
-      avatar.style.left = `${hMove}px`;
-    }
-  }
-  // down
-  if (event.keyCode === 40) {
-    if (aRect.top < window.innerHeight - offsetH) {
-      vMove += 50;
-      avatar.style.top = `${vMove}px`;
-    }
-  }
-
-  if (isTouching(avatar, coin)) {
-    sound.play();
-    score++;
-    init();
+      break;
+    case "ArrowUp":
+      mario.style.top = marioUp + "px";
+      marioY = marioUp;
+      console.log(marioY);
+      check();
+      break;
+    case "ArrowDown":
+      mario.style.top = marioDown + "px";
+      marioY = marioDown;
+      console.log(marioY);
+      check();
+      break;
   }
 });
+
+function check() {
+  if (marioX === coinX && marioY === coinY) {
+    coinMove();
+    console.log("Okay");
+    scoreCount += 1;
+    sound.play();
+  }
+}
+score.innerHTML = `Score: ${scoreCount}`;
+document.body.appendChild(score);
